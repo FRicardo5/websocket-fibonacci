b38206a3f301712db9e1496e69d40a5c13794f50
Ricardo <felipe.ferreira@bsatech.io>
2025-10-19 18:51:10 -0300

    chore: implementar funções para utilizar o banco de dados

--- app/config/settings.py
+++ app/config/settings.py
@@ -6,4 +6,6 @@ class Settings(BaseSettings):
         env_file='.env', env_file_encoding='utf-8'
     )
 
-    DATABASE_URL: str
+    DATABASE_URL: str = "postgresql://postgres:senha123@localhost:5432/websocket_fibonacci"
+    HOST: str = "0.0.0.0"
+    PORT: int = 8000
--- app/database/database.py
+++ app/database/database.py
@@ -1,11 +1,24 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, sessionmaker
 
 from app.config.settings import Settings
+from app.database.models.user import table_registry
 
 engine = create_engine(Settings().DATABASE_URL)
 
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
 
 def get_session():
-    with Session(engine) as session:
+    with SessionLocal() as session:
         yield session
+
+
+def create():
+    """Criar todas as tabelas do banco de dados"""
+    table_registry.metadata.create_all(bind=engine)
+
+
+def init():
+    """Iniciar o banco de dados"""
+    create()
+    print("Tabelas do banco de dados criadas com sucesso!")
