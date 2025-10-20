f721728c3cfd95249606f828a62ad30e914c6c84
Ricardo <felipe.ferreira@bsatech.io>
2025-10-19 22:21:38 -0300

    feat: implementa método para gerenciar as mensagens recebidas

--- app/server.py
+++ app/server.py
@@ -4,18 +4,20 @@ from app.database.models.user import User
 from app.database.database import get_session
 from sqlalchemy.orm import Session 
 import logging
+from datetime import datetime
+import json
 
 logger = logging.getLogger(__name__)    
 
 class WSManagerServer:
     def __init__(self):
-        self.clients: Dict[str, WebSocket] = {}
+        self.clients: Dict[int, WebSocket] = {}
         self.last_time_see = {}
 
-    async def connect(self, websocket: WebSocket, session: Session = Depends(get_session)):
+    async def connect(self, websocket: WebSocket, client_id: int, session: Session):
         await websocket.accept()
-        self.clients.append(websocket)
-        logger.info(f"Novo cliente conectado: {websocket.client.host}")
+        self.clients[client_id] = websocket
+        logger.info(f"Novo cliente conectado: {client_id}")
 
         try:
             contador = session.query(User).count()
@@ -31,20 +33,51 @@ class WSManagerServer:
             logger.error(f"erro: {e}")
 
 
-    def disconnect(self, websocket: WebSocket, client_id: int, session: Session = Depends(get_session)):
+
+    async def disconnect(self, websocket: WebSocket, client_id: int, session: Session) :
         if client_id in self.clients:
             try:
                 await websocket.close()
             except Exception as e:
                 pass
-            self.clients.pop(client_id)
+
             user = session.query(User).filter(User.id == client_id).first()
             user.connected = False
             user.disconnected_at = datetime.now()
             session.commit()
             session.refresh(user)
-            logger.error(f"Cliente {client_id} desconectado")
-        self.clients.remove(websocket)
+
+            self.clients.pop(client_id)
+            logger.info(f"{client_id} desconectado")
+
+
+    async def handle_messages(self, websocket: WebSocket, client_id: int, session: Session):
+        if client_id not in self.clients:
+            logger.error(f"Cliente {client_id} não encontrado")
+            return
+
+        data = await websocket.receive_text()
+        try:
+            data = json.loads(data)
+            print(f"Mensagem recebida {client_id}: {data}")
+
+            try:
+                msg_type = data["type"]
+
+                if msg_type == "fibonacci":
+                    n = int(data["n"])
+                    if n >= 0:
+                        await self._send_fibonacci(client_id, n)
+                    else:
+                        await self.clients[client_id].send_json({
+                            "type": "error",
+                            "message": "Número deve ser não-negativo"
+                        })
+            except Exception as e:
+                logger.error(f"erro: {e}")
+        except Exception as e:
+            logger.error(f"erro: {e}")
+           
 
     async def broadcast(self, message: str):
         for connection in self.clients:
